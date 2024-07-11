6168ef5b3972fb71308ed7bbbe8ae10bb9b29d25
Suprit-Iti <suprit.iti@ibm.com>
2024-07-11 18:09:49 +0530

    add missing funcs for handler

--- config/peerpods/podvm/libvirt-podvm-image-handler.sh
+++ config/peerpods/podvm/libvirt-podvm-image-handler.sh
@@ -11,7 +11,7 @@
 set -x
 # include common functions from lib.sh
 # shellcheck source=/dev/null
-# The directory is where aws-podvm-image-handler.sh is located
+# The directory is where libvirt-podvm-image-handler.sh is located
 source /scripts/lib.sh
 
 # Function to verify that the required variables are set
@@ -30,7 +30,6 @@ function verify_vars() {
     [[ -z "${CAA_REF}" ]] && error_exit "CAA_REF is empty"
 
     # Ensure booleans are set
-    [[ -z "${INSTALL_PACKAGES}" ]] && error_exit "INSTALL_PACKAGES is empty"
     [[ -z "${DOWNLOAD_SOURCES}" ]] && error_exit "DOWNLOAD_SOURCES is empty"
 }
 
@@ -40,17 +39,6 @@ function create_libvirt_image() {
     echo "Creating libvirt qcow2 image"
 
     # If any error occurs, exit the script with an error message
-
-    # Install packages if INSTALL_PACKAGES is set to yes
-
-    if [[ "${INSTALL_PACKAGES}" == "yes" ]]; then
-
-        # Install required rpm packages
-        install_rpm_packages
-
-        # Install required binary packages
-        install_libvirt_binary_packages
-    fi
 
     if [[ "${DOWNLOAD_SOURCES}" == "yes" ]]; then
         # Download source code from GitHub
@@ -67,52 +55,130 @@ function create_libvirt_image() {
     cd "${CAA_SRC_DIR}"/src/cloud-api-adaptor/podvm || \
 	    error_exit "Failed to change directory to ${CAA_SRC_DIR}/src/cloud-api-adaptor/podvm"
     make image
-    cp -pr /src/cloud-api-adaptor/src/cloud-api-adaptor/podvm/output/*.qcow2 /src/cloud-api-adaptor/src/cloud-api-adaptor/podvm/output/libvirt-podvm.qcow2
-    #mkdir -p /src/cloud-api-adaptor/src/cloud-api-adaptor/podvm/output
-    export PODVM_IMAGE_PATH=/src/cloud-api-adaptor/src/cloud-api-adaptor/podvm/output/libvirt-podvm.qcow2
+
+    export PODVM_IMAGE_PATH=/payload/podvm-libvirt.qcow2
+    cp -pr /src/cloud-api-adaptor/src/cloud-api-adaptor/podvm/output/*.qcow2 $PODVM_IMAGE_PATH
+
+    # Upload the created qcow2 to the volume
+    upload_libvirt_image
+
+    # Get the libvirt pool uuid
+    # This will set the LIBVIRT_POOL_UUID environment variable
+    get_libvirt_pool_uuid
+
+    # Add the ami id as annotation to peer-pods-cm configmap
+    add_libvirt_uuid_annotation_to_peer_pods_cm
+}
+
+# Function to upload the qcow2 image to volume
+
+function upload_libvirt_image() {
     echo "LIBVIRT_VOL_NAME: $LIBVIRT_VOL_NAME" && echo "LIBVIRT_POOL: $LIBVIRT_POOL" && \
 	    echo "LIBVIRT_URI: $LIBVIRT_URI" && echo "PODVM_IMAGE_PATH: $PODVM_IMAGE_PATH" 
     echo "Starting to upload the image."
     virsh -d 0 -c $LIBVIRT_URI vol-upload --vol $LIBVIRT_VOL_NAME $PODVM_IMAGE_PATH --pool $LIBVIRT_POOL --sparse
     if [ $? -eq 0 ]; then
-    echo "Uploaded the image successfully" 
-    LIBVIRT_POOL_UUID=$(virsh -d 0 -c $LIBVIRT_URI pool-list --name --uuid | grep $LIBVIRT_POOL | awk '{print $1}')
-    echo "Updating peer-pods-cm configmap with IMAGE_ID=${LIBVIRT_POOL_UUID}" 
-    else 
-    echo "Couldn't upload the image." 
+        echo "Uploaded the image successfully"
     fi
 }
 
+# Function to get the libvirt pood uuid of the newly created image
+
+function get_libvirt_pool_uuid() {
+    echo "Getting the libvirt pool uuid"
+
+    # Get the libvirt uuid of the uploaded qcow2 image
+    # If any error occurs, exit the script with an error message
+
+    LIBVIRT_POOL_UUID=$(virsh -d 0 -c $LIBVIRT_URI pool-list --name --uuid | grep $LIBVIRT_POOL | awk '{print $1}') ||
+        error_exit "Failed to get the libvirt uuid"
+
+    # Set the libvirt uuid as an environment variable
+    export LIBVIRT_POOL_UUID
+
+    echo "UUID of the libvirt pool: ${LIBVIRT_POOL_UUID}"
+}
+
+# Function to add the libvirt pool uuid as annotation in the peer-pods-cm configmap
+
+function add_libvirt_uuid_annotation_to_peer_pods_cm(){
+    echo "Adding libvirt uuid to peer-pods-cm configmap"
+
+    # Check if the peer-pods-cm configmap exists
+    if ! kubectl get configmap peer-pods-cm -n openshift-sandboxed-containers-operator >/dev/null 2>&1; then
+        echo "peer-pods-cm configmap does not exist. Skipping adding the libvirt uuid"
+        return
+    fi
+
+    # Add the libvirt pool uuid as annotation to peer-pods-cm configmap
+    # Overwrite any existing values
+    kubectl annotate --overwrite configmap peer-pods-cm -n openshift-sandboxed-containers-operator \
+        "LIBVIRT_POOL_UUID=${LIBVIRT_POOL_UUID}" ||
+        error_exit "Failed to add the libvirt uuid as annotation to peer-pods-cm configmap"
+
+    echo "Libvirt pool id added as annotation to peer-pods-cm configmap successfully"
+
+}
 
 # function to delete the libvirt
 # libvirt_id must be set as an environment variable
 
-function delete_libvirt_using_id() {
+function delete_libvirt_image_using_uuid() {
     echo "Deleting Libvirt image"
 
-    # Delete the AMI
+    # Delete the Libvirt pool 
     # If any error occurs, exit the script with an error message
 
-    # LIBVIRT_ID shouldn't be empty
-    [[ -z "${LIBVIRT_ID}" ]] && error_exit "LIBVIRT_ID is empty"
+    # LIBVIRT_POOL_UUID shouldn't be empty
+    [[ -z "${LIBVIRT_POOL_UUID}" ]] && error_exit "LIBVIRT_POOL_UUID is empty"
 
-    # Delete the libvirt image
+    # Delete the libvirt pool
+    echo "Deleting libvirt pool."
+    virsh -d 0 -c $LIBVIRT_URI pool-destroy $LIBVIRT_POOL_UUID --sparse ||
+        error_exit "Failed to destroy the libvirt pool"
+    
+    virsh -d 0 -c $LIBVIRT_URI pool-undefine $LIBVIRT_POOL_UUID --sparse ||
+        error_exit "Failed to undefine the libvirt pool"
+
+    echo "Uploaded the image successfully"
+
+    # Remove the ami id annotation from peer-pods-cm configmap
+    delete_libvirt_uuid_annotation_from_peer_pods_cm
+
+}
+
+# Function to delete the LIBVIRT_POOL_UUID annotation from the peer-pods-cm configmap
+
+function delete_libvirt_uuid_annotation_from_peer_pods_cm() {
+    echo "Deleting ami id annotation from peer-pods-cm configmap"
+
+    # Check if the peer-pods-cm configmap exists
+    if ! kubectl get configmap peer-pods-cm -n openshift-sandboxed-containers-operator >/dev/null 2>&1; then
+        echo "peer-pods-cm configmap does not exist. Skipping deleting the ami id"
+        return
+    fi
+
+    # Delete the libvirt uuid annotation from peer-pods-cm configmap
+    kubectl annotate configmap peer-pods-cm -n openshift-sandboxed-containers-operator \
+        "LIBVIRT_POOL_UUID" ||
+        error_exit "Failed to delete the ami id annotation from peer-pods-cm configmap"
+
+    echo "libvirt uuid annotation deleted from peer-pods-cm configmap successfully"
 }
 
 # display help message
 
 function display_help() {
     echo "This script is used to create libvirt qcow2 for podvm"
-    echo "Usage: $0 [-c|-C] [-- install_binaries|install_rpms|install_cli]"
+    echo "Usage: $0 [-c|-C] [-- install_binaries]"
     echo "Options:"
     echo "-c  Create image"
     echo "-C  Delete image"
-    echo "-R Recreate podvm-images configMap"
 }
 
 function install_packages(){
-    if [[ -n "${ACTIVATION_KEY}" && -n "${ORG_ID}" ]]; then \
-    subscription-manager register --org=${ORG_ID} --activationkey=${ACTIVATION_KEY}; \
+    if [[ -n "${ACTIVATION_KEY}" && -n "${ORG_ID}" ]]; then
+        subscription-manager register --org=${ORG_ID} --activationkey=${ACTIVATION_KEY}; \
     fi
 
     subscription-manager repos --enable codeready-builder-for-rhel-9-${ARCH}-rpms; \
@@ -135,16 +201,16 @@ function install_packages(){
     go version
 
 # Install packer. Packer doesn't does not have prebuilt s390x arch binaries above Packer version 0.1.5 
-    if [ "${ARCH}" == "s390x" ]; then \
-    git clone --depth 1 --single-branch https://github.com/hashicorp/packer.git -b ${PACKER_VERSION}; \
-    cd packer; \
-    sed -i -- "s/ALL_XC_ARCH=.*/ALL_XC_ARCH=\"${ARCH}\"/g" scripts/build.sh; \
-	sed -i -- "s/ALL_XC_OS=.*/ALL_XC_OS=\"Linux\"/g" scripts/build.sh; \
-    make bin && cp bin/packer /usr/local/bin/; \
-    cd $OLDPWD; \
-    else \
-    yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo && \
-    dnf install -y packer; \
+    if [ "${ARCH}" == "s390x" ]; then
+        git clone --depth 1 --single-branch https://github.com/hashicorp/packer.git -b ${PACKER_VERSION}
+        cd packer
+        sed -i -- "s/ALL_XC_ARCH=.*/ALL_XC_ARCH=\"${ARCH}\"/g" scripts/build.sh
+	    sed -i -- "s/ALL_XC_OS=.*/ALL_XC_OS=\"Linux\"/g" scripts/build.sh
+        make bin && cp bin/packer /usr/local/bin/
+        cd $OLDPWD
+    else
+        yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo && \
+        dnf install -y packer
     fi
 
     # set a correspond qemu-system-* named link to qemu-kvm
@@ -177,30 +243,22 @@ if [ "$1" = "--" ]; then
     install_binaries)
         install_packages
         ;;
-    install_rpms)
-        install_rpm_packages
-        ;;
     *)
         echo "Unknown argument: $1"
         exit 1
         ;;
     esac
 else
-    while getopts "cCRh" opt; do
+    while getopts "cCh" opt; do
         verify_vars
         case ${opt} in
         c)
-            # Create the ami
+            # Create the libvirt image
             create_libvirt_image
             ;;
         C)
-            # Delete the ami
-            delete_libvirt_using_id
-
-            ;;
-        R)
-            # Recreate the podvm-images configmap
-            recreate_image_configmap
+            # Delete the libvirt image
+            delete_libvirt_image_using_uuid
             ;;
         h)
             # Display help
